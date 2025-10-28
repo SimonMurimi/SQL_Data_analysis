fde172ef7643fe11f47e427a9abfd4ad13c90125
SimonMurimi <murrayirungu@gmail.com>
2025-10-27 17:51:04 -0700

    push example

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -1,42 +1,38 @@
-/* ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
-Database Load Issues (follow if receiving permission denied when running SQL code below)
+/*SELECT 
+job_title_short title,
+job_location AS Location,
+job_posted_date  AT TIME ZONE 'UTC'AT TIME ZONE 'EST' AS Date,
+EXTRACT(MONTH FROM job_posted_date) AS Datee
+FROM job_postings_fact
+LIMIT 10;*/
 
-NOTE: If you are having issues with permissions. And you get error: 
+/*SELECT *
+FROM(
+SELECT *
+FROM job_postings_fact
+WHERE EXTRACT(MONTH FROM job_posted_date) = 1
 
-'could not open file "[your file path]\job_postings_fact.csv" for reading: Permission denied.'
+)AS Simeon;*/
 
-1. Open pgAdmin
-2. In Object Explorer (left-hand pane), navigate to `sql_course` database
-3. Right-click `sql_course` and select `PSQL Tool`
-    - This opens a terminal window to write the following code
-4. Get the absolute file path of your csv files
-    1. Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
-5. Paste the following into `PSQL Tool`, (with the CORRECT file path)
+/*WITH simon AS (
+    SELECT *
+    FROM 
+    job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date) = 1
 
-\copy company_dim FROM '[Insert File Path]/company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+) 
+SELECT*
+FROM simon;*/
 
-\copy skills_dim FROM '[Insert File Path]/skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+SELECT 
+    company_id,
+    name AS company_name
+FROM company_dim
+WHERE company_id IN (
+SELECT 
+    company_id
+FROM job_postings_fact
+WHERE 
+    job_no_degree_mention = true
+)
 
-\copy job_postings_fact FROM '[Insert File Path]/job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-\copy skills_job_dim FROM '[Insert File Path]/skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-*/
-
--- NOTE: This has been updated from the video to fix issues with encoding
-
-COPY company_dim
-FROM 'C:\Users\Admin\Desktop\SQL_Data_analysis\csv_files\company_dim.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-COPY skills_dim
-FROM 'C:\Users\Admin\Desktop\SQL_Data_analysis\csv_files\skills_dim.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-COPY job_postings_fact
-FROM 'C:\Users\Admin\Desktop\SQL_Data_analysis\csv_files\job_postings_fact.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-COPY skills_job_dim
-FROM 'C:\Users\Admin\Desktop\SQL_Data_analysis\csv_files\skills_job_dim.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
